f0aafe6bab60951aabac4c46935612920f6754a0
dongxuefei <dongxf35@chinaunicom>
2021-03-31 15:29:01 +0800

    'first'

---
+++ order-server/src/main/java/is/seata/sample/service/OrderService.java
@@ -0,0 +1,26 @@
+package is.seata.sample.service;
+
+import io.seata.sample.entity.Order;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Mr.D
+ */
+public interface OrderService {
+
+    /**
+     * 创建订单
+     * @param order
+     * @return
+     */
+    void create(Order order);
+
+    /**
+     * 修改订单状态
+     * @param userId
+     * @param money
+     * @param status
+     */
+    void update(Long userId,BigDecimal money,Integer status);
+}
